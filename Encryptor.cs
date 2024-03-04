28adc0025888d8d05899b08dbadbb99521c135f9
Kieran Emery <kizwiz@hotmail.co.uk>
2024-03-04 15:36:08 +0000

    The encryption logic is encapsulated within the Encryptor class, which has a single responsibility of encrypting messages. The decryption logic is encapsulated within the Decryptor class, which has a single responsibility of decrypting messages. Both Encryptor and Decryptor classes use a common private method Transform to perform the actual transformation based on the provided key. The Program class no longer contains encryption or decryption logic; it only orchestrates the use of Encryptor and Decryptor classes.

---
+++ Encryptor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaesarCipher
+{
+    internal class Encryptor
+    {
+        private char[] alphabet = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+
+        public char[] Encrypt(char[] secretMessage)
+        {
+            return Transform(secretMessage, 3);
+        }
+
+        private char[] Transform(char[] message, int key)
+        {
+            char[] transformedMessage = new char[message.Length];
+
+            for (int i = 0; i < message.Length; i++)
+            {
+                char letter = message[i];
+                if (char.IsLetter(letter))
+                {
+                    int letterPosition = Array.IndexOf(alphabet, letter);
+                    int newLetterPosition = (letterPosition + key + 26) % 26; // Adding 26 to handle negative numbers
+                    char letterEncoded = alphabet[newLetterPosition];
+                    transformedMessage[i] = letterEncoded;
+                }
+                else
+                {
+                    transformedMessage[i] = letter;
+                }
+            }
+
+            return transformedMessage;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,6 +4,9 @@
     {
         static void Main(string[] args)
         {
+            Encryptor encryptor = new Encryptor();
+            Decryptor decryptor = new Decryptor();
+
             char[] alphabet = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
 
             Console.WriteLine("Enter your secret message:");
@@ -11,47 +14,14 @@
             char[] secretMessage = input.ToCharArray();
 
             // Encrypt the message
-            char[] encryptedMessage = Encrypt(alphabet, secretMessage);
+            char[] encryptedMessage = encryptor.Encrypt(secretMessage);
             string encodedString = new string(encryptedMessage);
             Console.WriteLine($"Your encoded message is: {encodedString}.");
 
             // Decrpyt the message
-            char[] decryptedMessage = Decrypt(alphabet, encryptedMessage);
+            char[] decryptedMessage = decryptor.Decrypt(encryptedMessage);
             string decodedString = new string(decryptedMessage);
             Console.WriteLine($"Your decoded message is: {decodedString}.");
-        }
-
-        private static char[] Encrypt(char[] alphabet, char[] secretMessage)
-        {
-            return Transform(alphabet, secretMessage, 3);
-        }
-
-        private static char[] Decrypt(char[] alphabet, char[] encryptedMessage)
-        {
-            return Transform(alphabet, encryptedMessage, -3);
-        }
-
-        private static char[] Transform(char[] alphabet, char[] message, int key)
-        {
-            char[] transformedMessage = new char[message.Length];
-
-            for (int i = 0; i < message.Length; i++)
-            {
-                char letter = message[i];
-                if (char.IsLetter(letter))
-                {
-                    int letterPosition = Array.IndexOf(alphabet, letter);
-                    int newLetterPosition = (letterPosition + key + 26) % 26; // Adding 26 to handle negative numbers
-                    char letterEncoded = alphabet[newLetterPosition];
-                    transformedMessage[i] = letterEncoded;
-                }
-                else
-                {
-                    transformedMessage[i] = letter;
-                }
-            }
-
-            return transformedMessage;
         }
     }
 }
