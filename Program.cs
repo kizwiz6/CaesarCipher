5a23ad80ea241825d2939f6e7f258e507b02b84a
Kieran Emery <kizwiz@hotmail.co.uk>
2024-03-04 15:12:04 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace CaesarCipher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] alphabet = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+            Console.WriteLine("Enter your secret message:");
+            string input = Console.ReadLine();
+            char[] secretMessage = input.ToCharArray();
+
+            char[] encryptedMessage = new char[secretMessage.Length];
+
+            for (int i = 0; i < secretMessage.Length; i++)
+            {
+                char letter = secretMessage[i];
+                int letterPosition = Array.IndexOf(alphabet, letter);
+                int newLetterPosition = (letterPosition + 3) % 26;
+                char letterEncoded = alphabet[newLetterPosition];
+                encryptedMessage[i] = letterEncoded;
+            }
+
+            string encodedString = String.Join("", encryptedMessage);
+            Console.WriteLine($"Your encoded message is: {encodedString}.");
+        }
+    }
+}
