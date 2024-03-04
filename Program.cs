1dff1bf4118034d976fd7984051446f0023258c3
Kieran Emery <kizwiz@hotmail.co.uk>
2024-03-04 15:15:44 +0000

    The app didn't work with uppercase letters. This update fixes that by converting any message to lowercase.

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@
             char[] alphabet = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
 
             Console.WriteLine("Enter your secret message:");
-            string input = Console.ReadLine();
+            string input = Console.ReadLine().ToLower();
             char[] secretMessage = input.ToCharArray();
 
             char[] encryptedMessage = new char[secretMessage.Length];
